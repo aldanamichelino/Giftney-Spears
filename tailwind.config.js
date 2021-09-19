7a399d76c9daee013b319b69ea8a39e28562ba67
aldana.michelino <aldana.michelino@gmail.com>
2021-09-19 15:01:00 -0300

    primer entregable

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      pink: colors.fuchsia,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
